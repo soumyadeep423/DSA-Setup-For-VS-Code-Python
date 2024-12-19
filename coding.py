75692479896d4c30df0f6b0a37323f4da32a0ff3
Soumyadeep <113242256+soumyadeep423@users.noreply.github.com>
2024-12-20 00:09:48 +0530

    Add files via upload

---
+++ coding.py
@@ -0,0 +1,10 @@
+import sys
+from collections import defaultdict
+sys.stdin = open("C:/Varun/DSA (Python)/input.txt","r")
+sys.stdout = open("C:/Varun/DSA (Python)/output.txt", "w")
+
+# Start Coding From Here
+# Inputs
+
+# Approach/Algorithm
+
